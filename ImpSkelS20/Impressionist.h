f5f658404dde9b63ede522bf7d629c855cda3230
Johnny Lo <klloaj@connect.ust.hk>
2021-02-02 16:06:23 +0800

    initial commit

---
+++ ImpSkelS20/Impressionist.h
@@ -0,0 +1,18 @@
+#ifndef IMPRESSIONIST_H
+#define IMPRESSIONIST_H
+#define SAMPLE_SOLUTION
+#include <FL/Fl.h>
+#include <FL/gl.h>
+#include <OpenGL/glu.h>
+#include <stdio.h>
+
+#include "ImpBrush.h"
+
+#define TRUE 1
+#define FALSE 0
+
+#ifdef WIN32
+#define M_PI 3.1415926535F
+#endif
+
+#endif
---
+++ ImpSkelS20/README.md
@@ -0,0 +1,1 @@
+do (brew install fltk) first
