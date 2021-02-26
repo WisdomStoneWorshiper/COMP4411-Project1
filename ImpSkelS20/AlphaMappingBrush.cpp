ece7d62d6dbd40887d5f9584b09fd236063a548a
Kinshuk Kotecha <kotechakinshuk@gmail.com>
2021-02-26 18:57:39 +0800

    alpha clipping done

--- ImpSkelS20/AlphaMappingBrush.cpp
+++ ImpSkelS20/AlphaMappingBrush.cpp
@@ -30,8 +30,8 @@ void AlphaMappingBrush::BrushMove(const Point source, const Point target) {
 
     if (!pDoc->m_ucAlphaMap) return;
 
-    int start_point_x = source.x - pDoc->m_nAlphaWidth/2;
-    int start_point_y = source.y - pDoc->m_nAlphaHeight/2;
+    int start_point_x = target.x - pDoc->m_nAlphaWidth/2;
+    int start_point_y = target.y - pDoc->m_nAlphaHeight/2;
     float original_alpha = pDoc->getAlpha();
 
 	glBegin(GL_POINTS);
@@ -39,8 +39,10 @@ void AlphaMappingBrush::BrushMove(const Point source, const Point target) {
     for (int i = 0; i < 3*pDoc->m_nAlphaHeight*pDoc->m_nAlphaWidth; i+=3) {
         float alpha = (pDoc->m_ucAlphaMap[i]+pDoc->m_ucAlphaMap[i+1]+pDoc->m_ucAlphaMap[i+2])/765.f;
         pDoc->setAlpha(alpha);
-        SetColor(source);
-        glVertex2d(start_point_x+(i/3 % pDoc->m_nAlphaWidth), start_point_y+(i/(3*pDoc->m_nAlphaWidth)));
+        Point pos;
+        pos.x = start_point_x+(i/3 % pDoc->m_nAlphaWidth);
+        pos.y = start_point_y+(i/(3*pDoc->m_nAlphaWidth));
+        ClipBrushStroke(source, pos);
     }
 
 	glEnd();
@@ -48,7 +50,15 @@ void AlphaMappingBrush::BrushMove(const Point source, const Point target) {
     pDoc->setAlpha(original_alpha);
 }
 
-void AlphaMappingBrush::ClipBrushStroke(const Point source, const Point target) {}
+void AlphaMappingBrush::ClipBrushStroke(const Point source, const Point target) {
+    ImpressionistDoc* pDoc = GetDocument();
+	if (target.x >= 0 && target.y >= 0 && pDoc->m_pUI->m_paintView->get_m_nDrawWidth() >= target.x
+		&& target.y >= pDoc->m_pUI->m_paintView->get_m_nWindowHeight() - pDoc->m_pUI->m_paintView->get_m_nDrawHeight()
+		&& pDoc->m_pUI->m_paintView->get_m_nWindowHeight() >= target.y) {
+		SetColor(source);
+		glVertex2d(target.x, target.y);
+	}
+}
 
 void AlphaMappingBrush::BrushEnd(const Point source, const Point target) {
 	// do nothing so far
